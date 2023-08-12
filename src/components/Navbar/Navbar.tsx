00d412ddb8f3bcab62597c7262dbf98dca360a4e
hazadus <hazadus7@gmail.com>
2023-08-12 21:33:15 +0300

    Added logos to Navbar.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Flex, Image } from "@chakra-ui/react";
 
 const Navbar: React.FC = () => {
-  return <div>bredit</div>;
+  return (
+    <>
+      <Flex bg="white" height="44px" padding="6px 12px">
+        <Flex align="center">
+          <Image src="/images/breditLogo.svg" height="30px" />
+          <Image src="/images/breditText.svg" height="46px" display={{ base: "none", md: "unset" }} />
+        </Flex>
+      </Flex>
+    </>
+  );
 };
 
 export default Navbar;
