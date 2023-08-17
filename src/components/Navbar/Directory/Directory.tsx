46b270016b65f9a57e8159af7ff00d5da6fe1917
hazadus <hazadus7@gmail.com>
2023-08-18 01:45:04 +0300

    Directory dropdown in Navbar prepared.

---
+++ src/components/Navbar/Directory/Directory.tsx
@@ -0,0 +1,38 @@
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import { Flex, Icon, Menu, MenuButton, MenuList, Text } from "@chakra-ui/react";
+import React from "react";
+import { TiHome } from "react-icons/ti";
+
+const Directory: React.FC = () => {
+  return (
+    <>
+      <Menu>
+        <MenuButton
+          cursor="pointer"
+          padding="0 6px"
+          ml={{ base: 0, md: 2 }}
+          mr={2}
+          borderRadius={4}
+          _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
+        >
+          <Flex align="center" justify="space-between" width={{ base: "auto", lg: "200px" }}>
+            <Flex align="center">
+              <Icon as={TiHome} fontSize={24} mr={{ base: 1, md: 2 }} />
+              <Flex display={{ base: "none", lg: "flex" }}>
+                <Text fontSize="10pt" fontWeight={600}>
+                  Home
+                </Text>
+              </Flex>
+            </Flex>
+            <Flex>
+              <ChevronDownIcon />
+            </Flex>
+          </Flex>
+        </MenuButton>
+        <MenuList></MenuList>
+      </Menu>
+    </>
+  );
+};
+
+export default Directory;
