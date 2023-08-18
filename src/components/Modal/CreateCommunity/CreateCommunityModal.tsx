bfb5f2291f12512048af9253a445ffddb7de1d2b
hazadus <hazadus7@gmail.com>
2023-08-19 01:43:13 +0300

    Add community snippets subdocument on user document when new community is created.

--- src/components/Modal/CreateCommunity/CreateCommunityModal.tsx
+++ src/components/Modal/CreateCommunity/CreateCommunityModal.tsx
@@ -17,7 +17,7 @@ import {
   Stack,
   Text,
 } from "@chakra-ui/react";
-import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { doc, getDoc, runTransaction, serverTimestamp, setDoc } from "firebase/firestore";
 import React, { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { BsFillEyeFill, BsFillPersonFill } from "react-icons/bs";
@@ -65,21 +65,30 @@ const CreateCommunityModal: React.FC<Props> = ({ isOpen, handleClose }) => {
     setIsLoading(true);
 
     try {
-      // Check if community with this name already exists.
       const communityDocRef = doc(firestore, "communities", communityName);
-      const communityDoc = await getDoc(communityDocRef);
 
-      if (communityDoc.exists()) {
-        setValidationError(`Sorry, b/${communityName} is taken. Try another.`);
-        return;
-      }
+      await runTransaction(firestore, async (transaction) => {
+        // Check if community with this name already exists
+        const communityDoc = await transaction.get(communityDocRef);
 
-      // Create community document in the Firestore
-      await setDoc(communityDocRef, {
-        creatorId: user?.uid,
-        createdAt: serverTimestamp(),
-        numberOfMembers: 1,
-        privacyType: communityType,
+        if (communityDoc.exists()) {
+          setValidationError(`Sorry, b/${communityName} is taken. Try another.`);
+          return;
+        }
+
+        // Create community document in the Firestore
+        await transaction.set(communityDocRef, {
+          creatorId: user?.uid,
+          createdAt: serverTimestamp(),
+          numberOfMembers: 1,
+          privacyType: communityType,
+        });
+
+        // Create community snippets subdocument in user document
+        transaction.set(doc(firestore, `users/${user?.uid}/communitySnippets`, communityName), {
+          communityId: communityName,
+          isModerator: true,
+        });
       });
     } catch (error: any) {
       console.log("Error creating community:", error);
