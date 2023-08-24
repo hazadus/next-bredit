f6c004a138e56b0c077e71e9b7d88a0982955259
hazadus <hazadus7@gmail.com>
2023-08-24 22:35:21 +0300

    ui: Added some links.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,11 @@
 import { auth } from "@/firebase/clientApp";
 import { Flex, Image } from "@chakra-ui/react";
+import Link from "next/link";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import Directory from "./Directory/Directory";
 import RightContent from "./RightContent/RightContent";
 import SearchInput from "./SearchInput";
-import Directory from "./Directory/Directory";
 
 const Navbar: React.FC = () => {
   const [user, loading, userError] = useAuthState(auth);
@@ -13,7 +14,9 @@ const Navbar: React.FC = () => {
     <>
       <Flex bg="white" height="44px" padding="6px 12px" justify={{ md: "space-between" }}>
         <Flex align="center" width={{ base: "40px", md: "auto" }} mr={{ base: 0, md: 2 }}>
-          <Image src="/images/breditLogo.svg" height="30px" />
+          <Link href="/">
+            <Image src="/images/breditLogo.svg" height="30px" />
+          </Link>
           <Image src="/images/breditText.svg" height="46px" display={{ base: "none", md: "unset" }} />
         </Flex>
         {user && <Directory />}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import { Flex, Text } from "@chakra-ui/react";
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -6,6 +8,17 @@ export default function Home() {
       <Head>
         <title>Bredit</title>
       </Head>
+      <Flex justify="center" p="16px 0px">
+        <Flex p={5} width="95%" maxWidth="974px" direction="column">
+          <Text fontWeight={600}>Some hard-coded links to communities:</Text>
+          <Link href="/b/hazadus">
+            <Text _hover={{ textDecoration: "underline" }}>/b/hazadus</Text>
+          </Link>
+          <Link href="/b/MagicTheGathering">
+            <Text _hover={{ textDecoration: "underline" }}>/b/MagicTheGathering</Text>
+          </Link>
+        </Flex>
+      </Flex>
     </div>
   );
 }
