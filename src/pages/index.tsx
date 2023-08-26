d6765a1848dd30b923cff7b886467b5f272d26e5
hazadus <hazadus7@gmail.com>
2023-08-27 02:02:33 +0300

    ui: Added community list on app index page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
+import { firestore } from "@/firebase/clientApp";
+import { ICommunity } from "@/types/types";
 import { Flex, Text } from "@chakra-ui/react";
+import { collection, getDocs, query } from "firebase/firestore";
 import Head from "next/head";
 import Link from "next/link";
+import React from "react";
+import safeJsonStringify from "safe-json-stringify";
 
-export default function Home() {
+type HomePageProps = {
+  communities: ICommunity[];
+};
+
+const HomePage: React.FC<HomePageProps> = ({ communities }) => {
   return (
     <div>
       <Head>
@@ -10,15 +19,30 @@ export default function Home() {
       </Head>
       <Flex justify="center" p="16px 0px">
         <Flex p={5} width="95%" maxWidth="974px" direction="column">
-          <Text fontWeight={600}>Some hard-coded links to communities:</Text>
-          <Link href="/b/hazadus">
-            <Text _hover={{ textDecoration: "underline" }}>/b/hazadus</Text>
-          </Link>
-          <Link href="/b/MagicTheGathering">
-            <Text _hover={{ textDecoration: "underline" }}>/b/MagicTheGathering</Text>
-          </Link>
+          <Text fontWeight={600} pb={2}>
+            We have these communities:
+          </Text>
+          {communities.map((comm) => (
+            <Link href={`/b/${comm.id}`}>
+              <Text _hover={{ textDecoration: "underline" }}>/b/{comm.id}</Text>
+            </Link>
+          ))}
         </Flex>
       </Flex>
     </div>
   );
-}
+};
+
+export default HomePage;
+
+export const getServerSideProps = async () => {
+  const communityDocuments = await getDocs(query(collection(firestore, "communities")));
+  const communities = communityDocuments.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+
+  return {
+    props: {
+      // JSON stringify/parse stuff because Next doesn't like Firebase's Timstamp type
+      communities: JSON.parse(safeJsonStringify(communities)),
+    },
+  };
+};
