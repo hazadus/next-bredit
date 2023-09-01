74b3e0c3a9227b4797f84cc6a03fee343a5683ac
hazadus <hazadus7@gmail.com>
2023-09-01 10:25:27 +0300

    dev: Added tracking code for stats.hazadus.ru.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,27 @@ import { theme } from "@/chakra/theme";
 import Layout from "@/components/Layout/Layout";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { RecoilRoot } from "recoil";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <ChakraProvider theme={theme}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ChakraProvider>
-    </RecoilRoot>
+    <>
+      <Head key="_app.tsx">
+        <script
+          async
+          src="https://stats.hazadus.ru/script.js"
+          data-website-id="a3cffe8d-da26-4f7b-88b3-eeb62b5ea845"
+        ></script>
+      </Head>
+      <RecoilRoot>
+        <ChakraProvider theme={theme}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ChakraProvider>
+      </RecoilRoot>
+    </>
   );
 }
 
