54e5f0c8d5835cc8130d17902ea30fbcbef7a9b2
hazadus <hazadus7@gmail.com>
2023-09-03 03:30:19 +0300

    ci/cd: Sentry installed.

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://392e5e9667913cff59e9fd9b9f2c89f8@o1402378.ingest.sentry.io/4505813661450240",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
